1b0aed52fae5624cd35d38fcc1044e12499af807
Abdur Rahman <47577116+khalilahmad1547@users.noreply.github.com>
2019-12-06 18:56:04 +0500

    Best Enigma Till now

--- Rottor.cc
+++ Rottor.cc
@@ -1,39 +1,51 @@
 Rottor::Rottor()
 {
-    Key=0;
-    TempArray=NULL;
-}
-Rottor::Rottor(int k,char* a)
-{
-
-    Key=k; //setting the Key
-   AlphaArray=a; //setting the address of the AlphaArray
+	Key=0;
+	TempCount=0;
+	Count=0;
+	cout<<"Enter the Rottor Number "<<endl;
+	cin>>RottorNo;
+    char a[26]={ 'w','r','y','i','p','k','h','f','s','z','c','b','m','q', 'n','e','v','t','x','u','a','o','d','l','g','j' };
+    AlphaArray=new char[strlen(a)]; //setting the address of the AlphaArray
+   strcpy(AlphaArray,a);    //copying the data from a to TempArray
    TempArray=new char[strlen(a)]; //creating a new memory with length same as AlphaArray is
-   strcpy(TempArray,a); //copying the data from AlphaArray to TempArray
+   strcpy(TempArray,a);     //copying the data from a to TempArray
+   SetRottor(); //calling SetRottor function to set TempArray according to the Key
+}
+Rottor::Rottor(int k,char* a,int RNo)
+{
+    RottorNo=RNo;
+    Count=0;
+    TempCount=0;
+    Key=k; //setting the Key
+   AlphaArray=new char[strlen(a)]; //setting the address of the AlphaArray
+   strcpy(AlphaArray,a); //copying the data from a to TempArray
+   TempArray=new char[strlen(a)]; //creating a new memory with length same as AlphaArray is
+   strcpy(TempArray,a); //copying the data from a to TempArray
    SetRottor(); //calling SetRottor function to set TempArray according to the Key
 }
 void Rottor::Rotate(int time,bool Dir)
 {
-
+    int Size=strlen(TempArray);
    	if(Dir==true) //if true rotate Clock Wise
 	{
 		//rotating clock wise for time 'time'
         for(int i=0;i<time;i++)
             {
                 char temp=TempArray[0];
-                for(int i=0;i<26;i++)
+                for(int i=0;i<Size;i++)
                 {
                 TempArray[i]=TempArray[i+1];
                 }
                 TempArray[25]=temp;
             }
     }
-	else if(Dir==false)//rotating Anti-ClockWise
+	 if(Dir==false)//rotating Anti-ClockWise
 	{
      for(int i=0;i<time;i++)
         {
             char temp=TempArray[25];
-            for(int i=25;i>=0;i--)
+            for(int i=Size;i>=0;i--)
             {
                 TempArray[i]=TempArray[i-1];
             }
@@ -44,16 +56,129 @@ void Rottor::Rotate(int time,bool Dir)
 
 char Rottor::Encrypt(char ch)
 {
-    Rotate(1,true); //rotating one time clock wise
-    return GetCharA(GetPositionT(ch));
-}
 
+if(RottorNo==1)// 1st Rottor rotates every time it Encrypt
+{
+
+    if(TempCount==0)//first half cycle of encryption
+    {
+
+        TempCount++;
+    Rotate(1,true); //rotating one time clock wise
+    return GetCharT(GetPositionA(ch));
+    }
+    else if(TempCount==1)
+    {
+    TempCount--;//in secod half cycle we doses not rotate the rottor
+    return GetCharA(GetPositionT(ch));
+    }
+}
+if(RottorNo==2)
+{
+ if(TempCount==0)
+   {
+
+     if(Count==26)
+     {
+     Rotate(1,true);
+     Count=0;
+     }
+        Count++;
+     TempCount++;
+     return GetCharT(GetPositionA(ch));
+   }
+   else if(TempCount==1)
+   {
+     TempCount--;
+     return GetCharA(GetPositionT(ch));
+   }
+
+}
+if(RottorNo==3)
+{
+
+   if(TempCount==0)
+   {
+
+     if(Count==52)
+     {
+     Rotate(1,true);
+     Count=0;
+     }
+     Count++;
+     TempCount++;
+     return GetCharT(GetPositionA(ch));
+   }
+   else if(TempCount==1)
+   {
+     TempCount--;
+     return GetCharA(GetPositionT(ch));
+   }
+}
+}
+/*
 char Rottor::DeCrypt(char ch)
 {
-    Rotate(1,true);//rotating anti clock wise
- return GetCharT(GetPositionA(ch));//decrypting
+if(RottorNo==1)// 1st Rottor rotates every time it Encrypt
+{
+
+    if(TempCount==0)//first half cycle of encryption
+    {
+
+        TempCount++;
+    Rotate(1,false); //rotating one time clock wise
+    return GetCharT(GetPositionA(ch));
+    }
+    else if(TempCount==1)
+    {
+    TempCount--;
+    return GetCharA(GetPositionT(ch));
+    }
+}
+if(RottorNo==2)
+{
+ if(TempCount==0)
+   {
+
+     if(Count==26)
+     {
+     Rotate(1,false);
+     Count=0;
+     }
+        Count++;
+     TempCount++;
+     return GetCharT(GetPositionA(ch));
+   }
+   else if(TempCount==1)
+   {
+     TempCount--;
+     return GetCharA(GetPositionT(ch));
+   }
 
 }
+if(RottorNo==3)
+{
+
+   if(TempCount==0)
+   {
+
+     if(Count==52)
+     {
+     Rotate(1,true);
+     Count=0;
+     }
+     Count++;
+     TempCount++;
+     return GetCharA(GetPositionT(ch));
+   }
+   else if(TempCount==1)
+   {
+     TempCount--;
+     return GetCharT(GetPositionA(ch));
+   }
+}
+*/
+
 int Rottor::GetPositionT(char ch) //getting the present position of char ah in TempArray
 {
     for(int i=0;i<26;i++)
@@ -133,4 +258,5 @@ void Rottor::SetRottor()
 Rottor::~Rottor()
 {
 delete[] TempArray;
+delete[] AlphaArray;
 }
--- main.cpp
+++ main.cpp
@@ -1,14 +1,7 @@
 #include <iostream>
+using namespace std;
 #include<fstream>
 #include<cstring>
-/*
-#include "enigma.h"
-#include "Plugboard.cc"
-#include "Rotor1.cc"
-#include "Rotor2.cc"
-#include "Rotor3.cc"
-#include "Reflector.cc"
-*/
 #include "Rottor.h"
 #include "Rottor.cc"
 #include "plugboard.h"
@@ -16,152 +9,151 @@
 #include "Reflector.h"
 #include "reflector.cc"
 
-using namespace std;
 
-int main()
+
+namespace Enigmaa
 {
-    //################################################# File handling ##########################################//
-
-     ifstream fin;
-	fin.open("enigma.ini");
-	char ch, eny;
+    char ch, eny;
 	int i = 0,  R1, R2, R3;		// some random variable required
 	char *ptr;
-
-	try{
-		while (!fin.eof())
-			{
-				fin.get(ch);
-				if (ch != 13 || ch != 10 )
-				{
-					if(int(ch) == 10)
-						{
-						break;
-						}
-					else
-					i++;				// counting string length of message
-				}
-			}
-
-		ptr = new char [i];		// creating array memory on heap
-		fin.seekg(0);					// pointing cusor back to start in text file
-		int b= 0;
-
-		while (!fin.eof())
-		{
-			fin.get(ch);
-			if (ch != 13 || ch != 10 )
-			{
-				if(int(ch) == 10)
-				{
-					break;
-				}
-				else
-				{
-					ptr[b] = ch;
-					if (int(ch) == 48 || int(ch) == 49 || int(ch) == 50|| int(ch) == 51|| int(ch) == 52|| int(ch) == 53|| int(ch) == 54|| int(ch) == 55|| int(ch) == 56|| int(ch) == 57)
-						{
-							cout<<"inside throw block"<<endl;
-							throw 1;
-						}
-					b++;
-				}
-			}
-		}
-
-
-	//	cout<<i<<endl; 			// printing message length
-		cout<<ptr<<endl;		// printing message on console
-
-		fin.get(ch);			// getting character e/d
-		eny = ch;
-		cout<<"Ch for encryption is "<<eny<<endl;
-
-		fin.get(ch);			// getting character for next line
-		fin >> R1;				// getting Key1
-		if(R1 >= 26)
-			{
-				throw true;
-			}
-
-		fin.get(ch);
-		fin >> R2;
-		if(R2 >= 26)
-			{
-				throw 'a';
-			}
-
-		fin.get(ch);
-		fin >> R3;
-		if(R3 >= 26)
-			{
-				throw 4.00;
-			}
-
-		cout<<R1<<endl<<R2<<endl<<R3<<endl;
-	}
-
-	catch (int number )
-		{
-			cout<<"Number is not allowed in Encryption\n please change the input string\n, .... good byee.\n";
-		}
-
-	catch (bool R1)
-		{
-			cout<< "Key is greater than 26 for Rotor 1, kindy enter key less than 26 and restart the enigma....\n";
-		}
-
-	catch (char R2 )
-		{
-			cout<< "Key is greater than 26 for Rotor 2, kindy enter key less than 26 and restart the enigma....\n";
-		}
-
-	catch (float R3 )
-		{
-			cout<< "Key is greater than 26 for Rotor 3, kindy enter key less than 26 and restart the enigma....\n";
-		}
-		//############################################################//
-        char p[26]={ 'w','r','y','i','p','k','h','f','s','z','c','b','m','q', 'n','e','v','t','x','u','a','o','d','l','g','j' };
-		Plugboard A;
-        Reflector B;
-        Rottor C(R1,p);
-        Rottor D(R2,p);
-        Rottor E(R2,p);
-        char a;
-        cout << "String is "<<ptr<<endl<<"i is "<<i<<endl;
-        if(eny == 'e')
-        {
-             for(int y=0; y<i; y++)
+    void file_handling(char * arrq)
+    {
+         ifstream fin;
+         fin.open(arrq);
+         if (fin.fail())
             {
-                  a = A.swap(ptr[y]);
-                  a = C.Encrypt(a);
-                  a = D.Encrypt(a);
-                  a = E.Encrypt(a);
-                  a = B.swap(a);
-
-
-                  cout<< "After Encryption "<< a<<endl;
+                cout << "Couldn't find the argument \n   opening default ini file ...\n" << endl;
+                fin.open("enigma.ini");
             }
-        }
-        else if(eny == 'd')
-        {
-            C.SetRottor();
-             for(int y=0; y<i; y++)
+
+         while (!fin.eof())
             {
-                  a = B.swap(ptr[y]);
-                  a = E.DeCrypt(a);
-                  a = D.DeCrypt(a);
-                  a = C.DeCrypt(a);
-                  a = A.swap(a);
+                fin.get(ch);
+                if (ch != 13 || ch != 10 )
+                  {
+                     if(int(ch) == 10)
+                        {
+                            break;
+                        }
+                      else
+                          i++;				// counting string length of message
+                  }
+             }
+            i--;
+          ptr = new char [i];		// creating array memory on heap
+          fin.seekg(0);					// pointing cusor back to start in text file
+          int b= 0;
 
-                cout<< "After Dcryption "<< a<<endl;
+          while (!fin.eof())
+            {
+                fin.get(ch);
+                    if (ch != 13 || ch != 10 )
+                    {
+                        if(int(ch) == 10)
+                        {
+                            break;
+                        }
+                        else
+                        {
+                            ptr[b] = ch;
+                            if (int(ch) == 48 || int(ch) == 49 || int(ch) == 50|| int(ch) == 51|| int(ch) == 52|| int(ch) == 53|| int(ch) == 54|| int(ch) == 55|| int(ch) == 56|| int(ch) == 57)
+                                {
+                                    cout<<"Number in Message,... but program will handle it."<<endl;
+                                    //throw 1;
+                                }
+                            b++;
+                        }
+                    }
+             }
+        // cout<<ptr<<endl;		// printing message on console
 
+		 fin.get(ch);			// getting character e/d
+		 eny = ch;
+		// cout<<"Ch for encryption is "<<eny<<endl;
+
+		 fin.get(ch);			// getting character for next line
+		 fin >> R1;				// getting Key1
+		 fin.get(ch);
+		 fin >> R2;
+		 fin.get(ch);
+		 fin >> R3;
+		// cout<<R1<<endl<<R2<<endl<<R3<<endl;
+        // delete[] ptr;
+    }
+
+    void cipher()
+    {
+            char p[26]={ 'w','r','y','i','p','k','h','f','s','z','c','b','m','q', 'n','e','v','t','x','u','a','o','d','l','g','j' };
+            Plugboard A;
+            Reflector B;
+            Rottor C(R1,p, 1);
+            Rottor D(R2,p, 2);
+            Rottor E(R2,p, 3);
+            char a;
+            cout << "String is "<<ptr<<endl<<"Message length is "<<i<<endl<<endl;
+            if(eny == 'e')
+            {
+                cout<< "Encrypted message is \" ";
+                 for(int y=0; y<i; y++)
+                  {
+                      if(ptr[y]>= 'a' && ptr[y] <= 'z' )
+                        {
+                          a = A.swap(ptr[y]);
+                          a = C.Encrypt(a);
+                          a = D.Encrypt(a);
+                          a = E.Encrypt(a);
+                          a = B.swap(a);
+                          a = E.Encrypt(a);
+                          a = D.Encrypt(a);
+                          a = C.Encrypt(a);
+                          a = A.swap(a);
+                          cout<< a;
+                        }
+                      else
+                        {
+                         cout<<ptr[y];
+                        }
+                  }
+               cout<<"\"";
+           }
+          else if(eny == 'd')
+           {
+                C.SetRottor();
+                cout<< "Decrypted message is \" ";
+                for(int y=0; y<i; y++)
+                 {
+                    if(ptr[y]>= 'a' && ptr[y] <= 'z' )
+                        {
+                          a = A.swap(ptr[y]);
+                          a = C.Encrypt(a);
+                          a = D.Encrypt(a);
+                          a = E.Encrypt(a);
+                          a = B.swap(a);
+                          a = E.Encrypt(a);
+                          a = D.Encrypt(a);
+                          a = C.Encrypt(a);
+                          a = A.swap(a);
+                          cout<< a;
+                         }
+                    else
+                        {
+                            cout<<ptr[y];
+                        }
+
+                  }
+                 cout<<"\""<<endl;
             }
-        }
+    }
 
-
-
-
-
+    //delete ptr;
 
 }
+
+
+int main(int argc, char ** argv)
+{
+    Enigmaa::file_handling(argv[1]);
+    Enigmaa::cipher();
+
+}
+
